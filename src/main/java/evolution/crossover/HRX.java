6d19977a6e7cd6338e3aaa320837facbea80c6e1
LuisaT-mgh <lui.thimm.19@lehre.mosbach.dhbw.de>
2022-04-25 17:52:01 +0200

    Changed hrx to fit current interface

--- src/main/java/evolution/crossover/HRX.java
+++ src/main/java/evolution/crossover/HRX.java
@@ -1,6 +1,7 @@
 package evolution.crossover;
 
 import evolution.IGene;
+import evolution.IGenotype;
 import evolution.Individuum;
 
 import java.security.DrbgParameters;
@@ -12,45 +13,100 @@ public class HRX extends CrossoverStrategy{
     public HRX(Random randomGenerator) {
         this.randomGenerator = randomGenerator;
     }
+    //TODO test
 
-    @Override
-    ArrayList<Individuum<?, ?>> execute(Individuum<?, ?> parent1, Individuum<?, ?> parent2) {
-        ArrayList<Individuum<?, ?>> children = new ArrayList<>();
+    private <T extends IGenotype<U>, U extends IGene> Individuum<T, U> getChildWithCossover(Individuum<T, U> parent1, Individuum<T, U> parent2) {
+        Individuum<T, U> returnIndividuum = parent1.createCopy();
 
-        children.add(getChildWithCossover(parent1, parent2));
-
-        return null;
-    }
-
-    private Individuum<?,?> getChildWithCossover(Individuum<?,?> parent1, Individuum<?,?> parent2) {
-        //TODO muss noch raus
-        Individuum<?, ?> returnIndividuum = null;
-        //TODO funktion in anderem commit erstellt
-        // Individuum<?, ?> returnIndividuum = parent1.createCopy;
-        ArrayList<IGene> remainingGenes = new ArrayList<>(parent1.getGenotype().getGenes());
+        ArrayList<U> remainingGenes = new ArrayList<>(parent1.getGenotype().getGenes());
         Collections.shuffle(remainingGenes);
 
-        Set<IGene> geneSet = new HashSet<>();
-        ArrayList<IGene> crossedOverGenes = new ArrayList<>();
-        IGene currentGene = null;
+        Set<U> geneSet = new HashSet<>();
+        ArrayList<U> crossedOverGenes = new ArrayList<>();
+        U currentGene = null;
         while(geneSet.size() != parent1.getGenotype().getGenes().size()){
             if(currentGene == null){
                 if(remainingGenes.size() == 0){
                     return mapCrossoverGenesToIndividuum(returnIndividuum, crossedOverGenes);
                 }
-                currentGene = getRandomGeneNotUsed(geneSet, remainingGenes);
+                currentGene = (U) getRandomGeneNotUsed(remainingGenes);
                 crossedOverGenes.add(currentGene);
+                geneSet.add(currentGene);
+            }
+            currentGene = getShortestEdgeGene(parent1, parent2, currentGene);
+            if(geneSet.contains(currentGene)){
+                currentGene = null;
+            }
+            else if(currentGene != null){
+                geneSet.add(currentGene);
+                crossedOverGenes.add(currentGene);
+                remainingGenes.remove(currentGene);
             }
         }
-
-
+        return  mapCrossoverGenesToIndividuum(returnIndividuum, crossedOverGenes);
     }
 
-    private Individuum<?,?> mapCrossoverGenesToIndividuum(Individuum<?,?> returnIndividuum, ArrayList<? extends IGene> crossedOverGenes) {
+    private <U extends IGene, T extends IGenotype<U>> U getShortestEdgeGene(Individuum<T,U> parent1, Individuum<T,U> parent2, U currentGene) {
+        HashMap<Integer, U> indexCity = new HashMap<>();
+        for(int i = 0; i<parent1.getGenotype().getGenes().size(); i++){
+            if(parent1.getGenotype().getGenes().get(i) == currentGene){
+                if(i+1 > parent1.getGenotype().getGenes().size()/2){
+                    if(i+1 < parent1.getGenotype().getGenes().size()){
+                        indexCity.put(parent1.getGenotype().getGenes().size()-i-1, parent1.getGenotype().getGenes().get(i + 1));
+                    }
+                }
+                else{
+                    if(i > 0){
+                        indexCity.put(i-1, parent1.getGenotype().getGenes().get(i-1));
+                    }
+                }
+            }
+            if(parent2.getGenotype().getGenes().get(i) == currentGene){
+                if(i+1 > parent2.getGenotype().getGenes().size()/2){
+                    if(i+1 < parent1.getGenotype().getGenes().size()) {
+                        indexCity.put(parent2.getGenotype().getGenes().size() - i - 1, parent2.getGenotype().getGenes().get(i + 1));
+                    }
+                }
+                else{
+                    if(i > 0)
+                        indexCity.put(i-1, parent2.getGenotype().getGenes().get(i-1));
+                }
+            }
+        }
+        if(indexCity.keySet().isEmpty()){
+            return null;
+        }
+        int min = Collections.min(indexCity.keySet());
+        return indexCity.get(min);
+    }
+
+    private <T extends IGenotype<U>, U extends IGene> U getRandomGeneNotUsed(ArrayList<U> remainingGenes) {
+        int index = this.randomGenerator.nextInt(remainingGenes.size());
+        U returnGene = remainingGenes.get(index);
+        remainingGenes.remove(index);
+        return returnGene;
+    }
+
+
+    private <T extends IGenotype<U>, U extends IGene> Individuum<T, U> mapCrossoverGenesToIndividuum(Individuum<T, U> returnIndividuum, ArrayList<U> crossedOverGenes) {
         for(int i = 0; i < crossedOverGenes.size(); i++){
             returnIndividuum.getGenotype().getGenes().set(i, crossedOverGenes.get(i));
         }
         return  returnIndividuum;
     }
 
+    @Override
+    protected <T extends IGenotype<U>, U extends IGene> ArrayList<Individuum<T, U>> executeInner(Individuum<T, U> parent1, Individuum<T, U> parent2) {
+        if (parent1.getGenotype().getGenes().get(0).getClass() != parent2.getGenotype().getGenes().get(0).getClass()) throw new RuntimeException("Crossover only works with parents that hold the same concrete type of gene!");
+
+        ArrayList<Individuum<T, U>> children = new ArrayList<>();
+
+        children.add(getChildWithCossover(parent1, parent2));
+        Individuum<T, U> compareIndividuum = getChildWithCossover(parent1, parent2);
+        while(children.get(0) == compareIndividuum){
+            compareIndividuum = getChildWithCossover(parent1, parent2);
+        }
+        children.add(compareIndividuum);
+        return children;
+    }
 }
